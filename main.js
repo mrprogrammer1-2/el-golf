26e4c1fd8528bb31fcfcd767ce065a7e89bb5081
mrprogrammer1-2 <sakatag681@gmail.com>
2024-12-11 21:48:43 +0200

    uu

--- main.js
+++ main.js
@@ -46,3 +46,20 @@ document.addEventListener("DOMContentLoaded", ()=> {
         }
     }
 })
+
+const btn = document.getElementById('up')
+
+window.addEventListener('scroll', ()=> {
+    if (window.scrollY >= 600) {
+        btn.style.display = "block";
+    }else {
+        btn.style.display = "none";
+    }
+})
+
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
+}
